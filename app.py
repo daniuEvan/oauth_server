3064064634054380deadbdc8383b65d532408571
liusaisai <big_ox@163.com>
2021-04-21 23:26:05 +0800

    create app

--- app.py
+++ app.py
@@ -2,9 +2,14 @@
 # -*- coding: utf-8 -*-
 # @time : 2021/4/21
 
-from website.app import App
+from website.app import create_app
 
-app = App()
+app = create_app(config={
+    'SECRET_KEY': 'secret',
+    'OAUTH2_REFRESH_TOKEN_GENERATOR': True,
+    'SQLALCHEMY_TRACK_MODIFICATIONS': False,
+    'SQLALCHEMY_DATABASE_URI': 'sqlite:///db.sqlite',
+})
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
